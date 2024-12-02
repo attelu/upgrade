1a19254561fc37ea59946c4b738f2caf6a0d2afe
eddy <eddy@stlwolf.com>
2024-12-03 03:25:37 +0900

    全部voidつくと面倒なので一旦スキップ

--- config/rector/config.php
+++ config/rector/config.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddReturnTypeDeclarationRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $services = $rectorConfig->services();
@@ -13,4 +14,8 @@ return static function (RectorConfig $rectorConfig): void {
 
     $services->load('Cake\\Upgrade\\Rector\\', __DIR__ . '/../..//src/Rector')
         ->exclude([__DIR__ . '/../../src/Rector/{Rector,ValueObject,Contract}']);
+
+    $rectorConfig->skip([
+        AddReturnTypeDeclarationRector::class,
+    ]);
 };
